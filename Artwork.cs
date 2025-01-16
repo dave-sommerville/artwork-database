58b91b0e0545f085d71c6e728f3f0be3e7119d0c
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-01-15 18:07:05 -0600

    Add project files.

---
+++ Artwork.cs
@@ -0,0 +1,31 @@
+﻿namespace Artwork_Database
+{
+    public class Artwork
+    {
+        public string Title;
+        public string Artist;
+        public string Year; //  May want to parse into an integer instead 
+        public string Medium;
+
+        public Artwork(string title, string artist, string year, string medium)
+        {
+            Title = title;
+            Artist = artist;
+            Year = year;
+            Medium = medium;
+        }
+        public static Artwork CreateObject(string[] splitInput)
+        {
+            if (splitInput.Length != 4)
+            {
+                Console.WriteLine("Array must have exactly four elements"); // Will want to convert to a throw error 
+            }
+            return new Artwork(splitInput[0], splitInput[1], splitInput[2], splitInput[3]);
+        }
+        public static string PrintArtwork(Artwork art)
+        {
+            string newLine = $"{art.Title} | {art.Artist} | {art.Year} | {art.Medium}";
+            return newLine;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Artwork_Database
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "Starry Night | Vincent van Gogh | 1889 | Painting";
+        }
+        public string[] SplitLine(string line)
+        {
+            string[] splitText = line.Split("|");
+            for (int i = 0; i < splitText.Length; i++)
+            {
+                splitText[i] = splitText[i].Trim();
+            }
+            return splitText;
+        }
+        //  Should definitely be private, possibly rework to work with a binary search 
+        public bool InsertAtNextIndex(string newString, string oldString)
+        {
+            int compResult = String.Compare(newString, oldString, StringComparison.OrdinalIgnoreCase);
+            if (compResult < 0 )
+            {
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+                                                                                //  Insert index will be a string with the value of the object targeted (Into its own array)
+        public string[] ObjectInsert(string newString, string[] stringArray, int insertIndex) 
+        {
+            string[] newArray = new string[stringArray.Length + 1];
+            for (int i = 0, j = 0; i < newArray.Length; i++)
+            {
+                if (i == insertIndex)
+                {
+                    newArray[i] = newString;
+                } else
+                {
+                    newArray[i] = stringArray[j];
+                    j++;
+                }
+            }
+            return newArray;
+        }
+
+
+    //  Accepting the file as an array of lines 
+    //  A snipper method (with trim)                                                x
+    //  Passes to Artwork method to construct objects                               x
+    //  A multiplier method to pass the object insert method to every array 
+    //  Comparision method to return sorting instruction                            x
+    //  Insertion method to adjust the array                                        x
+    //  File data has a for loop to insert sort every object into each array 
+    //  Menu interface will be needed 
+    //  User prompt to array method 
+    //  Binary search method (call upon comparision method) 
+    //  User menu to choose sorting criteria 
+    //  Output options 
+
+    //  Main issues with this are memory 
+    //  Also, should subcategories be sorted in some way? 
+
+
+}
+}
+
